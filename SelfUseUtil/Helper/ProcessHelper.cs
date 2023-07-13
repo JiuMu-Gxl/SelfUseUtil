a49105506ce115dae92b137038570d77483478b4
gao.xianglong <gao.xianglong@synyi.com>
2023-07-13 17:16:30 +0800

    feat: 添加进程操作类

---
+++ SelfUseUtil/Helper/ProcessHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfUseUtil.Helper
+{
+    /// <summary>
+    /// 进程工具类
+    /// </summary>
+    public class ProcessHelper
+    {
+        /// <summary>
+        /// 杀死进程
+        /// </summary>
+        /// <param name="processName">进程名称（不带exe）</param>
+        public void Kill(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length > 0)
+            {
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                    Console.WriteLine("进程 {0} 已终止。", process.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("没有找到该进程。");
+            }
+        }
+    }
+}
